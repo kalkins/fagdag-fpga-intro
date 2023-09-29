5820f6416a764d0843826e2436d685587ba2008b
Sindre Stephansen <sindre@sindrestephansen.com>
2023-09-29 18:05:56 +0200

    Rename task dir to be more descriptive

---
+++ task1_led/led.v
@@ -0,0 +1,19 @@
+module top(
+   input clk,
+   input rst_n,
+   output[7:0]led,
+);
+
+reg [32:0]  counter;
+
+assign led[7:0] = counter[31:24];
+
+always @(posedge clk) begin
+   if (rst_n) begin
+      counter <= counter + 1'b1;
+   end else begin
+      counter <= 0;
+   end
+end
+
+endmodule
