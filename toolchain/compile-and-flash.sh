3d473030a76e420fb1358254c4ebfd1fc0ff0b0b
Sindre Stephansen <sindre@sindrestephansen.com>
2023-09-29 18:06:27 +0200

    Compile all verilog files in the task dirs

--- toolchain/compile-and-flash.sh
+++ toolchain/compile-and-flash.sh
@@ -1,4 +1,4 @@
-yosys "-p synth_ice40 -top top -json top.json" top.v \
+yosys "-p synth_ice40 -top top -json top.json" src/*.v \
   && nextpnr-ice40 --hx8k --package cb132 --pcf cu.pcf --json top.json --asc top.asc \
   && icepack top.asc top.bin \
   && iceprog -d i:0x0403:0x6010:0 top.bin \
