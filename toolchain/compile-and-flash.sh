9c1efcb6a712fb66e601f1193e4af04787e11cae
Sindre Stephansen <sindre@sindrestephansen.com>
2023-10-06 14:19:12 +0200

    Make compiler/flasher take a input dir argument

--- toolchain/compile-and-flash.sh
+++ toolchain/compile-and-flash.sh
@@ -1,5 +1,8 @@
-yosys "-p synth_ice40 -top top -json top.json" src/top.v \
-  && nextpnr-ice40 --hx8k --package cb132 --pcf cu.pcf --json top.json --asc top.asc \
-  && icepack top.asc top.bin \
-  && iceprog -d i:0x0403:0x6010:0 top.bin \
+indir=$1
+outdir=$indir-out
+
+yosys "-p synth_ice40 -top top -json $outdir/top.json" $indir/top.v \
+  && nextpnr-ice40 --hx8k --package cb132 --pcf cu.pcf --json $outdir/top.json --asc $outdir/top.asc \
+  && icepack $outdir/top.asc $outdir/top.bin \
+  && iceprog -d i:0x0403:0x6010:0 $outdir/top.bin \
   && echo "Flash successful"
