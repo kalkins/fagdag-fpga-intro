00507320d6d42710d4f13a66b4e064ab4ccb197c
Sindre Stephansen <sindre@sindrestephansen.com>
2023-10-06 13:02:03 +0200

    Add absolute path for docker on mac

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 if [[ $# -ge 1 && -d $1 ]]; then
     devices=$(lsusb | awk '{ if ($6 == "0403:6010") { printf "--device=/dev/bus/usb/%s/%s ", $2, substr($4, 1, length($4)-1) } }')
 
-    docker build -t fpga-toolchain ./toolchain && docker run --rm -t -i $devices -v ./$1:/workspace/src -v ./cu.pcf:/workspace/cu.pcf fpga-toolchain
+    docker build -t fpga-toolchain ./toolchain && docker run --rm -t -i $devices -v $(pwd)/$1:/workspace/src -v $(pwd)/cu.pcf:/workspace/cu.pcf fpga-toolchain
 else
     echo "Compile and flash verilog to FPGA"
     echo "Usage: run.sh DIR"
